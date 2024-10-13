720802a4df39628bad64866d8ded284e4b98a704
saltimbanqui <saltimbanqui404@gmail.com>
2024-10-13 23:44:10 +0200

    projectBases

---
+++ glossary-service/src/test/java/com/kata/glossary_service/GlossaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.glossary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
